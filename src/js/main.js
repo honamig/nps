ce7523ade1df630d426475a5ce183f8a01df91ab
Honami Gonda <honamigonda38@gmail.com>
2026-01-22 18:42:54 -0700

    part 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import { setHeaderFooter } from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
-const parkData = getParkData();
 
 /* Main: intro */
 function setParkIntro(data) {
@@ -18,7 +17,15 @@ function setParkInfoLinks(data) {
   infoEl.insertAdjacentHTML("afterbegin", html);
 }
 
-/* Run */
-setHeaderFooter(parkData);
-setParkIntro(parkData);
-setParkInfoLinks(parkInfoLinks);
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
+
+init();
+
+
